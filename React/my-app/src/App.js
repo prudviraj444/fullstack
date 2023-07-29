5c6b531103494e57483f1ec55030bb417c07315d
unknown <prudhviraj.chavali@gmail.com>
2023-07-29 20:30:08 +0530

    react basics

---
+++ React/my-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import UsersComponent from './Components/Users/users';
+
+
+function App() {
+  return (
+    <div className="App">
+      <UsersComponent/>
+    </div>
+  );
+}
+
+
+export default App; 
