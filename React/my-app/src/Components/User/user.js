60c8fd403ce6597332bc3ae7dad9c1fbe59c7a17
unknown <prudhviraj.chavali@gmail.com>
2023-08-04 04:57:31 +0530

    useDetails Model and hooks

--- React/my-app/src/Components/User/user.js
+++ React/my-app/src/Components/User/user.js
@@ -1,20 +1,28 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import './user.css';
+
+
 function User(props){
-    const {data}=props;
-    return (
-        <Card className="userCard">
-      <Card.Img variant="top" src={data.picture} />
+  const {data,openModel}=props;
+
+
+  function onUserDetailsClick(){
+    openModel(data.id);
+  }
+
+  return (
+      <Card className="userCard">
+         <Card.Img variant="top" src={data.picture} />
       <Card.Body>
         <Card.Title> {  `${data.title} ${data.firstName} ${data.lastName}`   } </Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card's content.
         </Card.Text>
-        <Button variant="primary"> See Details </Button>
+        <Button onClick={onUserDetailsClick} variant="primary"> See Details </Button>
       </Card.Body>
     </Card>
     )
 }
-export default User;
+export default User;   
--- React/my-app/src/Components/Users1/users1.js
+++ React/my-app/src/Components/Users1/users1.js
@@ -1,6 +1,7 @@
 import React from "react";
 import User  from "../User/user";
 import Spinner from '../Commons/Spinner/spinner';
+import UserModel from '../UserModel/userModel';
 import './users.css';
 
 
@@ -8,7 +9,7 @@ class Users extends React.Component{
 
     constructor(){
         super();
-        this.state={isLoading:true,usersData:[],searchValue:""};
+        this.state={isLoading:true,usersData:[],searchValue:"",isModelOpen:false};
     }
 
     componentDidMount(){
@@ -22,45 +23,49 @@ class Users extends React.Component{
            this.completeData=data.data;
         })
     }
-
      showSpinner(){
-        return <Spinner/>
+         return <Spinner/>
     }
-
     onSearchFieldChange(e){
         const value=e.target.value.toLowerCase();
         this.setState({searchValue:value});
-
         const filteredData= this.completeData.filter((user)=>{
-           return  user.firstName.toLowerCase().startsWith(value);
+            return  user.firstName.toLowerCase().startsWith(value);
         })
-
         this.setState({usersData:filteredData});
     }
-
     showUsers(){
         return  <div>
+         <input onChange={(e)=>this.onSearchFieldChange(e)} value={this.state.searchValue} type="text" />
+         <div className="usersDiv">
 
-        <input onChange={(e)=>this.onSearchFieldChange(e)} value={this.state.searchValue} type="text" />
-        <div className="usersDiv">
+        {
+            this.state.usersData.map((user)=>{
+                return <User data={user} openModel={this.openModel.bind(this)} />
+            })
+        }
+        </div>
 
-       {
-           this.state.usersData.map((user)=>{
-               return <User data={user} />
-           })
-       }
+        </div>
+    }
+
+
+    openModel(id){
+        this.id=id;
+        this.setState({isModelOpen:true});
+    }
+
+    closeModel(){
+        this.setState({isModelOpen:false});
+    }
+
+
+    render(){
+        return <div>
+            <h1> Employee List</h1>
+            {(this.state.isLoading)?this.showSpinner() :this.showUsers()}
+            { this.state.isModelOpen &&  <UserModel  id={this.id}  closeModel={this.closeModel.bind(this)}  />}
        </div>
-
-       </div>
-   }
-
-
-   render(){
-       return <div>
-           <h1> Employee List</h1>
-
-           {(this.state.isLoading)?this.showSpinner() :this.showUsers()}
-      </div>
-   }
+    }
 }
 export default Users;
